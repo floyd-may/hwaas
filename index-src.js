86b8b57110fede3d595eb60665f33355de188b3f
Floyd May <floyd.may@gmail.com>
2016-03-28 21:58:26 -0500

    show version number and use npm packages

--- index-src.js
+++ index-src.js
@@ -1,8 +1,13 @@
 var http = require('http');
+var ejs = require('ejs');
+var fs = require('fs');
 
 var server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-  res.end("<html><body><h1>Hello World</h1></body></html>");
+  fs.readFile('index.html', (err, data) => {
+    var result = ejs.render(data.toString(), {version:'0.1'});
+    res.end(result);
+  });
 });
 
 server.listen(8000);
