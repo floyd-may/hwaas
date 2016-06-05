453e7004c94567f4a24dfd73e23fd902f51e4268
Floyd May <floyd.may@gmail.com>
2016-06-04 20:48:46 -0500

    show hostname in page

--- index-src.js
+++ index-src.js
@@ -1,11 +1,15 @@
 var http = require('http');
 var ejs = require('ejs');
 var fs = require('fs');
+var os = require('os');
 
 var server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
   fs.readFile('index.html', (err, data) => {
-    var result = ejs.render(data.toString(), {version:'0.1'});
+    var result = ejs.render(data.toString(), {
+        version:'0.1',
+        host: os.hostname()
+    });
     res.end(result);
   });
 });
