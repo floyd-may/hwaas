fc33250fe0b30b92a2926391f0f96047a8e72e93
Floyd May <floyd.may@gmail.com>
2016-09-02 16:44:38 -0500

    Update index-src.js

--- index-src.js
+++ index-src.js
@@ -7,7 +7,7 @@ var server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
   fs.readFile('index.html', (err, data) => {
     var result = ejs.render(data.toString(), {
-        version:'0.1',
+        version:'0.2',
         host: os.hostname()
     });
     res.end(result);
